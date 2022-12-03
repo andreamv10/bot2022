dc62975c30f4eb03d1faa290afe9734f80b7433e
andreamv10 <andreamv10@gmail.com>
2022-12-03 12:22:38 -0500

    sumar y dividir

--- bot.py
+++ bot.py
@@ -15,6 +15,56 @@ def on_command_start(message):
         "Hola, soy un \U0001F916, ¿cómo estás?",
         parse_mode="Markdown") 
     
+############ sumar ############# 
+
+@bot.message_handler(regexp=r"^sumar ([+-]?([0-9]*[.])?[0-9]+) y ([+-]?([0-9]*[.])?[0-9]+)$")
+def on_add(message):
+    bot.send_chat_action(message.chat.id, 'typing')
+    sleep(1)
+    parts = re.match(
+        r"^sumar ([+-]?([0-9]*[.])?[0-9]+) y ([+-]?([0-9]*[.])?[0-9]+)$",
+        message.text,
+        flags=re.IGNORECASE)
+    
+    #print (parts.groups())
+    
+    oper1 = float(parts[1])
+    oper2 = float(parts[3])
+    
+    result = oper1 + oper2
+    bot.reply_to(
+        message,
+        f"\U0001F522 Resultado: {result}")
+    
+############ dividir ############# 
+
+@bot.message_handler(regexp=r"^dividir ([+-]?([0-9]*[.])?[0-9]+) y ([+-]?([0-9]*[.])?[0-9]+)$")
+def on_dividir(message):
+    bot.send_chat_action(message.chat.id, 'typing')
+    sleep(1)
+    parts = re.match(
+        r"^dividir ([+-]?([0-9]*[.])?[0-9]+) y ([+-]?([0-9]*[.])?[0-9]+)$",
+        message.text,
+        flags=re.IGNORECASE)
+    
+    #print (parts.groups())
+    
+    oper1 = float(parts[1])
+    oper2 = float(parts[3])
+    
+    if oper2 == 0:
+        result= "no se puede dividir"
+    
+    else:
+        result = oper1 / oper2
+    
+    
+    bot.reply_to(
+        message,
+        f"\U0001F522 Resultado: {result}")
+
+############################################################################
+
 @bot.message_handler(func=lambda message: True)
 def on_fallback(message):
     bot.send_chat_action(message.chat.id, 'typing')
