1dff2bf5f1205b4bb1f719b77144890d5bc79b40
andreamv10 <andreamv10@gmail.com>
2022-12-03 11:29:23 -0500

    Added fallback block

--- bot.py
+++ bot.py
@@ -6,14 +6,23 @@ import re
 #########################################################
 # Aquí vendrá la implementación de la lógica del bot
 
-@bot.message_handler(commands=['ping'])
-def on_command_ping(message):
-    # bot.send_chat_action(message.chat.id, 'typing')
-# sleep(1)
+@bot.message_handler(commands=['start'])
+def on_command_start(message):
+    bot.send_chat_action(message.chat.id, 'typing')
+    sleep(1)
     bot.send_message(
         message.chat.id,
-        "pong",
+        "Hola, soy un \U0001F916, ¿cómo estás?",
         parse_mode="Markdown") 
+    
+@bot.message_handler(func=lambda message: True)
+def on_fallback(message):
+    bot.send_chat_action(message.chat.id, 'typing')
+    sleep(1)
+   
+    bot.reply_to(
+        message,
+        "\U0001F63F Ups, no entendí lo que me dijiste.")
 
 #########################################################
 if __name__ == '__main__':
